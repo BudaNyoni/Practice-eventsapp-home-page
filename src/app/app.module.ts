bdb177810134d39cbc5a57f9c23cd58d4f875823
BudaNyoni <nyonisbuda@gmail.com>
2018-10-25 16:47:38 +0200

    uyes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DisplayPage } from '../pages/display/display';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    DisplayPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +31,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    DisplayPage,
     TabsPage
   ],
   providers: [
